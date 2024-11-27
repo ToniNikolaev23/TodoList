1d9582e2baf3adff5d7f76ce27d2da408971ce65
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-11-27 21:16:13 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Toni Stoyanov on 27.11.24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
